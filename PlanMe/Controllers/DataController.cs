bafe9ffbc838ac7fa0723402119a7895c7a6c417
Proto68 <timkaq67@gmail.com>
2023-02-08 09:59:07 +0200

    Added file for database and made folders for MVC

---
+++ PlanMe/Controllers/DataController.cs
@@ -0,0 +1,10 @@
+using System.Data.SqlClient;
+
+namespace PlanMe
+{
+    //Class for basic work with the database
+    public class DataController
+    {
+        
+    }
+}
--- PlanMe/Form1.cs
+++ PlanMe/Form1.cs
@@ -6,5 +6,10 @@ namespace PlanMe
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
