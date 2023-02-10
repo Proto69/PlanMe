4b7957acd02561b8f64c21d967ecbcc4b75669f8
Proto68 <timkaq67@gmail.com>
2023-02-10 16:14:30 +0200

    Added the MySql.Data library and the connection string

--- PlanMe/Controllers/DataController.cs
+++ PlanMe/Controllers/DataController.cs
@@ -1,10 +1,12 @@
-using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace PlanMe
 {
     //Class for basic work with the database
     public class DataController
     {
-        
+        //The connection string
+        private string connectionString = Secrets.connectionString;
+
     }
 }
