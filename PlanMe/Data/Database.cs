011a1487b74f9991e6009c5d14e79e47e1b9a858
Proto68 <timkaq67@gmail.com>
2023-02-12 09:01:35 +0200

    Made new class to connect to the database.

---
+++ PlanMe/Data/Database.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanMe.Data
+{
+    public class Database
+    {
+        private string connectionString = Secrets.connectionString;
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
