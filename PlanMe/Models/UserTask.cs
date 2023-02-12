89552408736ee9266109528903bf39f9ab352546
Proto68 <timkaq67@gmail.com>
2023-02-12 08:44:53 +0200

    Added model for tasks..

---
+++ PlanMe/Models/UserTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanMe.Models
+{
+    public class UserTask
+    {
+        private string name;
+        private bool isDone;
+
+        public UserTask(string name)
+        {
+            this.Name = name;
+            IsDone = false;
+        }
+
+        public void Complete()
+        {
+            if (isDone)
+                throw new InvalidOperationException("This task is already done!");
+            isDone = true;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+        public bool IsDone
+        {
+            get { return isDone; }
+            set
+            {
+                isDone = value;
+            }
+        }
+    }
+}
