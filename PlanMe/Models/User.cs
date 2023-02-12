112848d134c34d027b7ddf15aaf36a3842b40989
Proto68 <timkaq67@gmail.com>
2023-02-12 15:43:49 +0200

    Updated the constructor and added methods.

--- PlanMe/Models/User.cs
+++ PlanMe/Models/User.cs
@@ -1,3 +1,4 @@
+using PlanMe.Data;
 using PlanMe.Models;
 
 namespace PlanMe
@@ -15,23 +16,37 @@ namespace PlanMe
         {
             this.Username = username;
             this.Password = password;
-            events = new List<Event>();
-            tasks = new List<UserTask>();
+            events = EventData.GetAll(username);
+            tasks = TaskData.GetAll(username);
         }
 
         public bool AddEvent(Event action)
         {
             throw new NotImplementedException();
         }
+
         public bool RemoveEvent(Event action)
         {
             throw new NotImplementedException();
         }
+
         public bool AddTask(UserTask task)
         {
             throw new NotImplementedException();
         }
+
         public bool RemoveTask(UserTask task)
+        {
+            throw new NotImplementedException();
+        }
+
+        //How to search these two
+        public Event GetEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserTask GetTask()
         {
             throw new NotImplementedException();
         }
