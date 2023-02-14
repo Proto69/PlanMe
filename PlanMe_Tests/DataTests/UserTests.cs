dbaa98a1195403da34df59eece3e63524069d665
Proto68 <timkaq67@gmail.com>
2023-02-14 13:39:10 +0200

    Added a method.

--- PlanMe_Tests/DataTests/UserTests.cs
+++ PlanMe_Tests/DataTests/UserTests.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PlanMe;
 
 namespace PlanMe_Tests.DataTests
 {
-    internal class UserTests
+    [TestFixture]
+    public class UserTests
     {
+        [Test]
+        public void UserCreateTest()
+        {
+            
+        }
     }
 }
