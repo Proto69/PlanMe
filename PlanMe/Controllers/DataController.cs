c828d42e1fbb1beca36438341e92a364ea6ec6df
Proto68 <timkaq67@gmail.com>
2023-02-20 16:04:52 +0200

    Added methods in Data Controller

--- PlanMe/Controllers/DataController.cs
+++ PlanMe/Controllers/DataController.cs
@@ -5,6 +5,23 @@ namespace PlanMe
 {
     public class DataController
     {
-        
+        //Creates user by getting a model from the view
+        public bool CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Updates user's password
+        public void UpdateUserPassword(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Deletes a user specified, from the view
+        public void DeleteUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
