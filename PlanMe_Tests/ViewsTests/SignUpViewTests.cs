7fb85449255a070c0d13f3f96bd9e352ddd710b0
Tsvetoslav <cveti3060@gmail.com>
2023-02-21 22:36:39 +0200

    Sign up view test done

---
+++ PlanMe_Tests/ViewsTests/SignUpViewTests.cs
@@ -0,0 +1,23 @@
+﻿using PlanMe;
+using PlanMe.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanMe_Tests.ViewsTests
+{
+    [TestFixture]
+    public class SignUpViewTests
+    {
+        [Test]
+        public void CheckIfReadsAndCreatesUser()
+        {
+            SignUpView.ReadInfo("Reads", "Read12", "Read12");
+            User user = UserData.Check("Reads", "Read12");
+            UserData.Delete("Read");
+            Assert.IsNotNull(user, "Does not reads!");
+        }
+    }
+}
