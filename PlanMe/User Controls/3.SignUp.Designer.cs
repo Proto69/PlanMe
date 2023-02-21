c4ae0b3ac25f298704069ddf09ab1032ff52156a
Proto68 <timkaq67@gmail.com>
2023-02-21 07:37:38 +0200

    Renamed elements from the UserControl.

--- PlanMe/User Controls/3.SignUp.Designer.cs
+++ PlanMe/User Controls/3.SignUp.Designer.cs
@@ -28,64 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.repeatPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.signUpButton = new System.Windows.Forms.Button();
+            this.logInLinkLabel = new System.Windows.Forms.LinkLabel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // nameTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(301, 235);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(300, 27);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "Enter name";
+            this.nameTextBox.Location = new System.Drawing.Point(301, 235);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(300, 27);
+            this.nameTextBox.TabIndex = 0;
+            this.nameTextBox.Text = "Enter name";
             // 
-            // textBox2
+            // passwordTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(302, 287);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(300, 27);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.Text = "Enter password";
+            this.passwordTextBox.Location = new System.Drawing.Point(302, 287);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(300, 27);
+            this.passwordTextBox.TabIndex = 1;
+            this.passwordTextBox.Text = "Enter password";
             // 
-            // textBox3
+            // repeatPasswordTextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(301, 342);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(300, 27);
-            this.textBox3.TabIndex = 2;
-            this.textBox3.Text = "Repeate password";
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.repeatPasswordTextBox.Location = new System.Drawing.Point(301, 342);
+            this.repeatPasswordTextBox.Name = "repeatPasswordTextBox";
+            this.repeatPasswordTextBox.Size = new System.Drawing.Size(300, 27);
+            this.repeatPasswordTextBox.TabIndex = 2;
+            this.repeatPasswordTextBox.Text = "Repeat password";
+            this.repeatPasswordTextBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // button1
+            // signUpButton
             // 
-            this.button1.BackColor = System.Drawing.Color.ForestGreen;
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(300, 412);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(301, 36);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Sign up";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.signUpButton.BackColor = System.Drawing.Color.ForestGreen;
+            this.signUpButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.signUpButton.Location = new System.Drawing.Point(300, 412);
+            this.signUpButton.Name = "signUpButton";
+            this.signUpButton.Size = new System.Drawing.Size(301, 36);
+            this.signUpButton.TabIndex = 3;
+            this.signUpButton.Text = "Sign up";
+            this.signUpButton.UseVisualStyleBackColor = false;
+            this.signUpButton.Click += new System.EventHandler(this.button1_Click);
             // 
-            // linkLabel1
+            // logInLinkLabel
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.LinkColor = System.Drawing.Color.Green;
-            this.linkLabel1.Location = new System.Drawing.Point(476, 186);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(50, 20);
-            this.linkLabel1.TabIndex = 4;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Log in";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.logInLinkLabel.AutoSize = true;
+            this.logInLinkLabel.LinkColor = System.Drawing.Color.Green;
+            this.logInLinkLabel.Location = new System.Drawing.Point(476, 186);
+            this.logInLinkLabel.Name = "logInLinkLabel";
+            this.logInLinkLabel.Size = new System.Drawing.Size(50, 20);
+            this.logInLinkLabel.TabIndex = 4;
+            this.logInLinkLabel.TabStop = true;
+            this.logInLinkLabel.Text = "Log in";
+            this.logInLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // label1
             // 
@@ -112,11 +112,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.logInLinkLabel);
+            this.Controls.Add(this.signUpButton);
+            this.Controls.Add(this.repeatPasswordTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.nameTextBox);
             this.Name = "_3";
             this.Size = new System.Drawing.Size(900, 500);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -127,11 +127,11 @@
 
         #endregion
 
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private Button button1;
-        private LinkLabel linkLabel1;
+        private TextBox nameTextBox;
+        private TextBox passwordTextBox;
+        private TextBox repeatPasswordTextBox;
+        private Button signUpButton;
+        private LinkLabel logInLinkLabel;
         private Label label1;
         private PictureBox pictureBox1;
     }
