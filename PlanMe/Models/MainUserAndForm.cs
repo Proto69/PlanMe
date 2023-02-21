15cf2d51f30954bde8de1d96d35a4b8d640c9482
Proto68 <timkaq67@gmail.com>
2023-02-21 18:36:50 +0200

    Added main instance for user and form.

---
+++ PlanMe/Models/MainUserAndForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanMe.Models
+{
+    public static class MainUserAndForm
+    {
+        public static User user;
+        public static MainForm form;
+    }
+}
