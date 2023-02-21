aee5e28a420996c0c53d3789418b946a0b07335b
Proto68 <timkaq67@gmail.com>
2023-02-21 11:49:03 +0200

    Restored the main form.

--- PlanMe/MainForm.Designer.cs
+++ PlanMe/MainForm.Designer.cs
@@ -28,34 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(12, 60);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(900, 300);
-            this.panel1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(395, 387);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 16);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel1);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -63,8 +42,5 @@
         }
 
         #endregion
-
-        private Panel panel1;
-        private Button button1;
     }
 }
