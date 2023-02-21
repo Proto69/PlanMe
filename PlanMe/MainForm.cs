961ac0a18b318d8fabeeca246b8617df70c99c30
Proto68 <timkaq67@gmail.com>
2023-02-21 20:40:18 +0200

    Loading sign up on app start.

--- PlanMe/MainForm.cs
+++ PlanMe/MainForm.cs
@@ -14,6 +14,7 @@ namespace PlanMe
         private void MainForm_Load(object sender, EventArgs e)
         {
             MainUserAndForm.form = this;
+            ShowSignUp();
         }
 
         public void ShowMainScreen()
