b8e7be9c54a76f057c45dfdc92d353c3efdd0c8a
Proto68 <timkaq67@gmail.com>
2023-02-25 07:56:25 +0200

    Added model for the ListView.

--- PlanMe/Models/MainModels.cs
+++ PlanMe/Models/MainModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PlanMe.User_Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,6 @@ namespace PlanMe.Models
     {
         public static User user;
         public static MainForm form;
-        public static UserTask task;
+        public static ExistingList lists;
     }
 }
