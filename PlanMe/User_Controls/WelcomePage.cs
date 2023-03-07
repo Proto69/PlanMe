11f8c71782179371c91db7d1149b1e87086bf173
Daniela4578 <d.kremendka@gmail.com>
2023-03-07 16:24:54 +0200

    test

--- PlanMe/User_Controls/WelcomePage.cs
+++ PlanMe/User_Controls/WelcomePage.cs
@@ -16,5 +16,10 @@ namespace PlanMe.User_Controls
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
