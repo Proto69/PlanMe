9c00ecf2f58bd7636ae9deadcb3be6853eedc1dc
Tsvetoslav <cveti3060@gmail.com>
2023-03-12 13:55:53 +0200

    AddAPlan add button done. Emo needs to check it!

--- PlanMe/User_Controls/AddAPlan.Designer.cs
+++ PlanMe/User_Controls/AddAPlan.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.DateBox = new System.Windows.Forms.TextBox();
+            this.InfoBox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -41,74 +41,82 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(203, 62);
+            this.label1.Location = new System.Drawing.Point(304, 93);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 46);
+            this.label1.Size = new System.Drawing.Size(253, 62);
             this.label1.TabIndex = 1;
             this.label1.Text = "Add a plan";
             // 
-            // textBox1
+            // NameBox
             // 
-            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(137, 148);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(306, 32);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "Name ";
+            this.NameBox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NameBox.Location = new System.Drawing.Point(206, 222);
+            this.NameBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(457, 42);
+            this.NameBox.TabIndex = 2;
+            this.NameBox.Text = "Name ";
             // 
-            // textBox2
+            // DateBox
             // 
-            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox2.Location = new System.Drawing.Point(140, 198);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(306, 32);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "Date";
+            this.DateBox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DateBox.Location = new System.Drawing.Point(210, 297);
+            this.DateBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DateBox.Name = "DateBox";
+            this.DateBox.Size = new System.Drawing.Size(457, 42);
+            this.DateBox.TabIndex = 3;
+            this.DateBox.Text = "Date";
             // 
-            // textBox3
+            // InfoBox
             // 
-            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox3.Location = new System.Drawing.Point(137, 253);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(306, 32);
-            this.textBox3.TabIndex = 4;
-            this.textBox3.Text = "About";
+            this.InfoBox.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.InfoBox.Location = new System.Drawing.Point(206, 380);
+            this.InfoBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.InfoBox.Name = "InfoBox";
+            this.InfoBox.Size = new System.Drawing.Size(457, 42);
+            this.InfoBox.TabIndex = 4;
+            this.InfoBox.Text = "About";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.ForestGreen;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(201, 313);
+            this.button1.Location = new System.Drawing.Point(302, 470);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(183, 47);
+            this.button1.Size = new System.Drawing.Size(274, 70);
             this.button1.TabIndex = 5;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(137, 98);
+            this.label2.Location = new System.Drawing.Point(206, 147);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(309, 20);
+            this.label2.Size = new System.Drawing.Size(463, 30);
             this.label2.TabIndex = 6;
             this.label2.Text = "__________________________________________________";
             // 
             // AddAPlan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::PlanMe.Properties.Resources.background;
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.InfoBox);
+            this.Controls.Add(this.DateBox);
+            this.Controls.Add(this.NameBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AddAPlan";
-            this.Size = new System.Drawing.Size(600, 400);
+            this.Size = new System.Drawing.Size(900, 600);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,9 +125,9 @@
         #endregion
 
         private Label label1;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
+        private TextBox NameBox;
+        private TextBox DateBox;
+        private TextBox InfoBox;
         private Button button1;
         private Label label2;
     }
--- PlanMe/User_Controls/AddAPlan.cs
+++ PlanMe/User_Controls/AddAPlan.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PlanMe.User_Controls
 {
@@ -15,6 +16,29 @@ namespace PlanMe.User_Controls
         public AddAPlan()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime date = new DateTime();
+                try
+                {
+                    date = DateTime.Parse(DateBox.Text);
+                }
+                catch(Exception ex) 
+                {
+                    ErrorView.ShowError(ex.Message);
+                }
+                string time = date.TimeOfDay.ToString();
+                Event currEvent = new Event(NameBox.Text, date, time, InfoBox.Text);
+                EventData.Upload(currEvent, MainModels.user.Username);
+            }
+            catch (Exception ex)
+            {
+                ErrorView.ShowError(ex.Message);
+            }
         }
     }
 }
