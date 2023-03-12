5113db9390e489ff04b7443f16a221c177b0e0d5
Tsvetoslav <cveti3060@gmail.com>
2023-03-12 13:32:41 +0200

    AddList create button done

--- PlanMe/User_Controls/AddList.cs
+++ PlanMe/User_Controls/AddList.cs
@@ -19,7 +19,15 @@ namespace PlanMe.User_Controls
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                ListOfTasksData.Upload(new ListOfTasks(textBox1.Text, MainModels.user.Username));
+                PageControl.ListsMainScreen();
+            }
+            catch (Exception ex)
+            {
+                ErrorView.ShowError(ex.Message);
+            }
         }
     }
 }
