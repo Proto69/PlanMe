65e97a2073fe6dc35e32b7dc577c7e4f0bae3494
Proto68 <timkaq67@gmail.com>
2023-03-14 17:41:26 +0200

    Some more.

--- PlanMe/ErrorForm.Designer.cs
+++ PlanMe/ErrorForm.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ErrorForm";
+            SuspendLayout();
+            // 
+            // ErrorForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(415, 203);
+            Name = "ErrorForm";
+            Text = "ErrorForm";
+            ResumeLayout(false);
         }
 
         #endregion
