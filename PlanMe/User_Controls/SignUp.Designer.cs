70f4b7a4dc1f81e1484532a439189ca2aee67a99
Tsvetoslav <cveti3060@gmail.com>
2023-03-26 01:30:25 +0200

    Sign up text size fixed

--- PlanMe/User_Controls/SignUp.Designer.cs
+++ PlanMe/User_Controls/SignUp.Designer.cs
@@ -43,32 +43,35 @@
             // 
             NameBox.Anchor = AnchorStyles.None;
             NameBox.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            NameBox.Location = new Point(300, 224);
+            NameBox.Location = new Point(450, 336);
+            NameBox.Margin = new Padding(4, 4, 4, 4);
             NameBox.Name = "NameBox";
             NameBox.PlaceholderText = "Enter username";
-            NameBox.Size = new Size(300, 30);
+            NameBox.Size = new Size(448, 39);
             NameBox.TabIndex = 0;
             // 
             // PasswordBox
             // 
             PasswordBox.Anchor = AnchorStyles.None;
             PasswordBox.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            PasswordBox.Location = new Point(300, 272);
+            PasswordBox.Location = new Point(450, 408);
+            PasswordBox.Margin = new Padding(4, 4, 4, 4);
             PasswordBox.Name = "PasswordBox";
             PasswordBox.PasswordChar = '*';
             PasswordBox.PlaceholderText = "Enter password";
-            PasswordBox.Size = new Size(300, 30);
+            PasswordBox.Size = new Size(448, 39);
             PasswordBox.TabIndex = 1;
             // 
             // RepeatPasswordBox
             // 
             RepeatPasswordBox.Anchor = AnchorStyles.None;
             RepeatPasswordBox.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            RepeatPasswordBox.Location = new Point(300, 321);
+            RepeatPasswordBox.Location = new Point(450, 482);
+            RepeatPasswordBox.Margin = new Padding(4, 4, 4, 4);
             RepeatPasswordBox.Name = "RepeatPasswordBox";
             RepeatPasswordBox.PasswordChar = '*';
             RepeatPasswordBox.PlaceholderText = "Repeat password";
-            RepeatPasswordBox.Size = new Size(300, 30);
+            RepeatPasswordBox.Size = new Size(448, 39);
             RepeatPasswordBox.TabIndex = 2;
             // 
             // SignUpButton
@@ -77,9 +80,10 @@
             SignUpButton.BackColor = Color.ForestGreen;
             SignUpButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             SignUpButton.ForeColor = SystemColors.ButtonHighlight;
-            SignUpButton.Location = new Point(300, 386);
+            SignUpButton.Location = new Point(450, 579);
+            SignUpButton.Margin = new Padding(4, 4, 4, 4);
             SignUpButton.Name = "SignUpButton";
-            SignUpButton.Size = new Size(300, 50);
+            SignUpButton.Size = new Size(450, 75);
             SignUpButton.TabIndex = 3;
             SignUpButton.Text = "Sign up";
             SignUpButton.UseVisualStyleBackColor = false;
@@ -92,9 +96,10 @@
             LogInLinkedLabel.BackColor = Color.Transparent;
             LogInLinkedLabel.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
             LogInLinkedLabel.LinkColor = Color.Green;
-            LogInLinkedLabel.Location = new Point(502, 176);
+            LogInLinkedLabel.Location = new Point(753, 264);
+            LogInLinkedLabel.Margin = new Padding(4, 0, 4, 0);
             LogInLinkedLabel.Name = "LogInLinkedLabel";
-            LogInLinkedLabel.Size = new Size(57, 23);
+            LogInLinkedLabel.Size = new Size(80, 32);
             LogInLinkedLabel.TabIndex = 4;
             LogInLinkedLabel.TabStop = true;
             LogInLinkedLabel.Text = "Log in";
@@ -106,9 +111,10 @@
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(300, 176);
+            label1.Location = new Point(450, 264);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(206, 23);
+            label1.Size = new Size(287, 32);
             label1.TabIndex = 8;
             label1.Text = "Already have an account?";
             // 
@@ -117,10 +123,11 @@
             pictureBox1.Anchor = AnchorStyles.None;
             pictureBox1.BackColor = Color.Transparent;
             pictureBox1.Image = Properties.Resources.SingUp;
-            pictureBox1.Location = new Point(345, 60);
+            pictureBox1.Location = new Point(518, 90);
+            pictureBox1.Margin = new Padding(4, 4, 4, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(203, 91);
-            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.Size = new Size(304, 136);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 9;
             pictureBox1.TabStop = false;
             // 
@@ -129,15 +136,16 @@
             label2.Anchor = AnchorStyles.None;
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
-            label2.Location = new Point(280, 140);
+            label2.Location = new Point(420, 210);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(339, 20);
+            label2.Size = new Size(508, 30);
             label2.TabIndex = 10;
             label2.Text = "_______________________________________________________";
             // 
             // SignUp
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.SignUpBG;
             BackgroundImageLayout = ImageLayout.Stretch;
@@ -150,8 +158,9 @@
             Controls.Add(RepeatPasswordBox);
             Controls.Add(PasswordBox);
             DoubleBuffered = true;
+            Margin = new Padding(4, 4, 4, 4);
             Name = "SignUp";
-            Size = new Size(900, 500);
+            Size = new Size(1350, 750);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
